d805633a726adeca875e85036bd808904b227ddf
MsFT100 <lennoxmathew@gmail.com>
2024-10-10 20:32:38 +0300

    rough work for website done

---
+++ static/js/navabar.js
@@ -0,0 +1,4 @@
+document.querySelector('.menuBtn').addEventListener('click', function() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('active'); // Toggle the active class to expand/collapse
+  });
